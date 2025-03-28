07a2dc34168fcd0922aa3c7d84dfd50d7234eecd
snehalds <sneds@eg.dk>
2025-03-28 11:44:37 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   <div className="bg-red-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe vel, obcaecati non ut voluptates quod consequuntur adipisci modi magnam id? Cum eligendi perferendis corrupti quisquam porro reprehenderit eius repudiandae cumque!</div>
+   </>
+  )
+}
+
+export default App
